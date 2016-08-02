c66704c98f939a24a04ee2131e376fbdb6c57b83
Scott Christensen <scottmc526@gmail.com>
2016-08-02 15:21:30 -0600

    stepOne Div

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('carefree_dental', ['ngRoute', 'ngResource']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/index.html',
+    controller: 'mainController'
+  })
+})
