9c03ae158fec079b7b831ee23bd83980fa9708e6
Scott Christensen <scottmc526@gmail.com>
2016-08-03 15:50:47 -0600

    refactor for maintainability

--- public/js/controllers/stepOneController.js
+++ public/js/controllers/stepOneController.js
@@ -1,18 +1,18 @@
 app.controller('stepOneController', function($scope) {
-  $scope.numberArray = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+  $scope.numberArray = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
   $scope.faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   $scope.warningMessage = true;
   $scope.stepTwoTable = false;
 
-  $scope.selectMembers = function(index) {
+  $scope.selectMembers = function(familySize) {
     $scope.familyArray = [];
     $scope.warningMessage = false;
     $scope.stepTwoTable = true;
 
-    index < 10 ? $scope.familyMembers = index + 1 : alert ('exceeded maximum family members');
+    familySize < 11 ? $scope.familyMembers = familySize : alert ('exceeded maximum family members');
 
     for (var i = 1; i <= $scope.familyMembers; i++) {
-      $scope.familyArray.push(i)
+      $scope.familyArray.push(i);
     }
   }
 })
