832542a9ac66eb1050d14fb927b1fee9711ae75f
Scott Christensen <scottmc526@gmail.com>
2016-08-02 18:10:40 -0600

    select members function

--- public/js/controllers/stepOneController.js
+++ public/js/controllers/stepOneController.js
@@ -1,6 +1,7 @@
 app.controller('stepOneController', function($scope) {
   $scope.faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-
+  $scope.selectMembers = function(index) {
+    console.log(index);
+  }
 })
