89ba23588cc1f3399ad9575f632379b5d80dc78a
Scott Christensen <scottmc526@gmail.com>
2016-08-03 15:10:33 -0600

    dynamic rows with correct person numbers

--- public/js/controllers/stepOneController.js
+++ public/js/controllers/stepOneController.js
@@ -1,17 +1,18 @@
 app.controller('stepOneController', function($scope) {
-  $scope.familyArray = [];
+  $scope.numberArray = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
   $scope.faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   $scope.warningMessage = true;
   $scope.stepTwoTable = false;
 
   $scope.selectMembers = function(index) {
+    $scope.personCounter = 0;
+    $scope.familyArray = [];
     $scope.warningMessage = false;
     $scope.stepTwoTable = true;
     $scope.familyMembers = index + 1
 
     for (var i = 1; i <= $scope.familyMembers; i++) {
       $scope.familyArray.push(i)
-
     }
   }
 })
