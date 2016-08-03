502b7d41c76ab315955337304515ae5fffe8a5eb
Scott Christensen <scottmc526@gmail.com>
2016-08-03 14:55:29 -0600

    table rows dynamically update

--- public/js/controllers/stepOneController.js
+++ public/js/controllers/stepOneController.js
@@ -1,4 +1,5 @@
 app.controller('stepOneController', function($scope) {
+  $scope.familyArray = [];
   $scope.faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   $scope.warningMessage = true;
   $scope.stepTwoTable = false;
@@ -6,5 +7,11 @@ app.controller('stepOneController', function($scope) {
   $scope.selectMembers = function(index) {
     $scope.warningMessage = false;
     $scope.stepTwoTable = true;
+    $scope.familyMembers = index + 1
+
+    for (var i = 1; i <= $scope.familyMembers; i++) {
+      $scope.familyArray.push(i)
+
+    }
   }
 })
