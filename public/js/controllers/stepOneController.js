fdfe1b9c063d3252a86249740d18f2d6f9a4ea3f
Scott Christensen <scottmc526@gmail.com>
2017-01-20 19:03:58 -0700

    function refactors

--- public/js/controllers/stepOneController.js
+++ public/js/controllers/stepOneController.js
@@ -5,30 +5,30 @@ app.controller('stepOneController', function($scope) {
   $scope.savingsArray = [];
 
   $scope.smileyFaces = [
-    {id: 1, className: 'unselected'},
-    {id: 2, className: 'unselected'},
-    {id: 3, className: 'unselected'},
-    {id: 4, className: 'unselected'},
-    {id: 5, className: 'unselected'},
-    {id: 6, className: 'unselected'},
-    {id: 7, className: 'unselected'},
-    {id: 8, className: 'unselected'},
-    {id: 9, className: 'unselected'},
-    {id: 10, className: 'unselected'},
+    { id: 1, className: 'unselected' },
+    { id: 2, className: 'unselected' },
+    { id: 3, className: 'unselected' },
+    { id: 4, className: 'unselected' },
+    { id: 5, className: 'unselected' },
+    { id: 6, className: 'unselected' },
+    { id: 7, className: 'unselected' },
+    { id: 8, className: 'unselected' },
+    { id: 9, className: 'unselected' },
+    { id: 10, className: 'unselected' },
   ];
 
   $scope.tableColumns = [
-    {name: 'Oral Exam', selected: false, savings: 100},
-    {name: 'Cleaning', selected: false, savings: 200},
-    {name: 'Filling', selected: false, savings: 150},
-    {name: 'X-Rays', selected: false, savings: 300},
-    {name: 'Root Canal', selected: false, savings: 50},
-    {name: 'Crown', selected: false, savings: 75},
-    {name: 'Upper Denture', selected: false, savings: 125},
-    {name: 'Braces', selected: false, savings: 250}
+    { name: 'Oral Exam', selected: false, savings: 100 },
+    { name: 'Cleaning', selected: false, savings: 200 },
+    { name: 'Filling', selected: false, savings: 150 },
+    { name: 'X-Rays', selected: false, savings: 300 },
+    { name: 'Root Canal', selected: false, savings: 50 },
+    { name: 'Crown', selected: false, savings: 75 },
+    { name: 'Upper Denture', selected: false, savings: 125 },
+    { name: 'Braces', selected: false, savings: 250 }
   ];
 
-  $scope.selectMembers = function(familySize) {
+  $scope.selectMembers = (familySize) => {
     $scope.familyArray = [];
     $scope.warningMessage = false;
     $scope.stepTwoTable = true;
@@ -40,7 +40,7 @@ app.controller('stepOneController', function($scope) {
     };
   };
 
-  $scope.highlightFaces = function(familySize) {
+  $scope.highlightFaces = (familySize) => {
     $scope.smileyFaces.forEach(face => {
       face['className'] = 'unselected';
     });
@@ -50,13 +50,13 @@ app.controller('stepOneController', function($scope) {
     };
   };
 
-  $scope.individualSavings = function(person ,column) {
+  $scope.individualSavings = (person, column) => {
     $scope.familyArray[person]['selected'] = true;
 
     $scope.familyArray[person]['savings'] += $scope.tableColumns[column]['savings'];
   };
 
-  $scope.combinedSavings = function() {
+  $scope.combinedSavings = () => {
     $scope.totalSavings = 0;
 
     for (var i = 0; i < $scope.familyArray.length; i++) {
@@ -64,7 +64,7 @@ app.controller('stepOneController', function($scope) {
     };
   };
 
-  $scope.movieTickets = function () {
+  $scope.movieTickets = () => {
     //average cost of movie ticket = $8.43 according to google
     $scope.tickets = 0;
     $scope.tickets = Math.floor($scope.totalSavings / 8.43)
